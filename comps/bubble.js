e7943c25bf3f5615fcf5c634e0373e72137a51d8
= <bbrowne14@my.bcit.ca>
2022-03-16 16:30:04 -0700

    finished changes

--- comps/bubble.js
+++ comps/bubble.js
@@ -5,12 +5,38 @@ var template_bubble = document.createElement("template"); //<template> </templat
 template_bubble.innerHTML = `
 <style>
 
+.bubble {
+    display: grid;
+    justify-content: right;
+    grid-auto-flow: column;
+    grid-column: 2 / 3;
+}
+
+.buble {
+    display: grid;
+    justify-content: left;
+    grid-auto-flow: column;
+    grid-column: 2 / 3;
+}
+
+.clothing {
+    display: grid;
+    justify-content: center;
+    grid-auto-flow: column;
+    grid-column: 2 / 3;
+  }
+
+
 img {
-    width: 250px;
-    height: 250px;
+    width: 330px;
+    height: 330px;
 }
 
 img.blank:hover {
+    filter: drop-shadow(5px 5px 5px #67664F);
+}
+
+img.empty:hover {
     filter: drop-shadow(5px 5px 5px #67664F);
 }
 
@@ -19,6 +45,12 @@ img.blank:hover {
 <div class="bubbles">
     <div class="bubble">
         <img class="blank" src="/imgs/bubble1.svg" onclick="myFunction()">
+    </div>
+    <div class="clothing">
+        <img src="/imgs/outfits.svg" class="clothing" width="500" height="400">
+    </div>
+    <div class="buble">
+        <img class="empty" src="/imgs/bubble3.svg" onclick="myFunction()">
     </div>
 </div>
 
@@ -39,11 +71,15 @@ class Bubble extends HTMLElement {
     connectedCallback(){
         this.shadowRoot.appendChild(template_bubble.content.cloneNode(true)); //use the template to make a clone
         this.shadowRoot.querySelector('.bubble > img:nth-child(1)').onclick = () => this.showbubble2();
+        this.shadowRoot.querySelector('.buble > img:nth-child(1)').onclick = () => this.showbubble4();
     }
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
     showbubble2(){
         this.shadowRoot.querySelector('.bubble > img:nth-child(1)').src= '/imgs/bubble2.svg';
+    }
+    showbubble4(){
+        this.shadowRoot.querySelector('.buble > img:nth-child(1)').src= '/imgs/bubble4.svg';
     }
 }
 
